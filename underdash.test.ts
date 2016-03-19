6b73b8e365a99127b178a61a4b0890bcc25d83e3
willchen90 <willchen90@gmail.com>
2016-03-19 12:56:25 -0700

    implement core underdash

--- underdash.test.ts
+++ underdash.test.ts
@@ -14,23 +14,180 @@ describe('Underdash', function() {
       _.each([1, 2, 3], double);
       expect(array).to.eql([2, 4, 6]);
     });
+    it("works for objects", function() {
+      const result = {};
+      let double = (value, key) => result[key] = value * 2;
+      _.each({a: 1, b: 2, c: 3}, double);
+      expect(result).to.eql({a: 2, b: 4, c: 6})
+    })
   });
 
   describe('map()', function() {
-
+    it("array", function(){
+      const double = x => x * 2
+      expect(_.map([1, 2, 3], double)).to.eql([2,4,6])
+    })
   });
 
   describe('reduce()', function() {
-
+    it("array", function(){
+      const sum = (acc, value) => acc += value
+      expect(_.reduce([1,2,3], sum)).to.eql(6)
+    });
   })
 
   describe('filter()', function() {
-
+    it("array", function(){
+      const odd = n => (n % 2 === 1)
+      expect(_.filter([1,2,3], odd)).to.eql([1, 3])
+    });
   })
 
   describe('reject()', function() {
+    it("array", function(){
+      const odd = n => (n % 2 === 1)
+      expect(_.reject([1,2,3], odd)).to.eql([2])
+    });
+  });
 
-  })
+  describe('every()', function() {
+    it("array", function(){
+      const odd = n => (n % 2 === 1)
+      expect(_.every([3,5,7], odd)).to.eql(true)
+      expect(_.every([3,5,7, 2], odd)).to.eql(false)
+    });
+  });
+
+  describe('some()', function() {
+    it("returns true if at least one truthy value from iteraee", function(){
+      const odd = n => (n % 2 === 1)
+      expect(_.some([3,5,7], odd)).to.eql(true)
+      expect(_.some([3,5,7, 2], odd)).to.eql(true)
+      expect(_.some([2, 4, 6], odd)).to.eql(false)
+    });
+  });
+ 
+  describe('clone()', function() {
+    it("array", function(){
+      const input = [1,2,3];
+      const clonedInput = _.clone(input);
+      expect(_.isEqual(input, clonedInput)).to.equal(true)
+    });
+    it("object", function(){
+      const input = {a: 1, b: 2, c: 3};
+      const clonedInput = _.clone(input);
+      expect(_.isEqual(input, clonedInput)).to.equal(true)
+    });
+  });
+ 
+  describe('cloneDeep()', function() {
+    it("simple array", function(){
+      const input = [1,2,3];
+      const clonedInput = _.cloneDeep(input);
+      expect(_.isEqual(input, clonedInput)).to.equal(true)
+    });
+    
+    it("simple object", function(){
+      const input = {a: 1, b: 2, c: 3};
+      const clonedInput = _.cloneDeep(input);
+      expect(_.isEqual(input, clonedInput)).to.equal(true)
+    });
+    
+    it("nested array", function(){
+      const input = [1,2,[3, 4, [5]]];
+      const clonedInput = _.cloneDeep(input);
+      expect(_.isEqual(input, clonedInput)).to.equal(true);
+    });
+    it("nested object", function(){
+      const input = {a: 1, b: {a: 2, c: 3}};
+      const clonedInput = _.cloneDeep(input);
+      expect(_.isEqual(input, clonedInput)).to.equal(true)
+      input.b.a = 1;
+      expect(_.isEqual(input, clonedInput)).to.equal(false)
+    });
+    it("nested array + object", function(){
+      const input = {a: 1, b: {a: 2, c: [3, 4, [5, {a: 1}]]}};
+      const clonedInput = _.cloneDeep(input);
+      expect(_.isEqual(input, clonedInput)).to.equal(true)
+      input.b.a = 1;
+      expect(_.isEqual(input, clonedInput)).to.equal(false)
+    });
+  });
+  
+  describe('isEqual()', function() {
+    it("simple array", function(){
+      expect(_.isEqual([1,2,3], [1,2,3])).to.equal(true);
+      expect(_.isEqual([1,2,3], [1,2])).to.equal(false);
+    });
+    
+    it("simple object", function(){
+      expect(_.isEqual({a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3})).to.equal(true);
+      expect(_.isEqual({a: 1}, {a: 2, c: 4})).to.equal(false);
+    });
+    
+    it("nested array", function(){
+      expect(_.isEqual([1,2,[3, 4, [5]]], [1,2,[3, 4, [5]]])).to.equal(true);
+      expect(_.isEqual([1,2,[3, 4, [5]]], [1,2,[3, 4, [5, 7]]])).to.equal(false);
+    });
+    it("nested object", function(){
+      expect(_.isEqual({a: 1, b: {a: 2, c: 3}}, {a: 1, b: {a: 2, c: 3}}))
+        .to.equal(true);
+      expect(_.isEqual({a: 1, b: {a: 2, c: 3}}, {a: 1, b: {a: 2}}))
+        .to.equal(false);
+    });
+    it("nested array + object", function(){
+      expect(_.isEqual({a: 1, b: {a: 2, c: [3, 4, [5, {a: 1}]]}}, {a: 1, b: {a: 2, c: [3, 4, [5, {a: 1}]]}}))
+        .to.equal(true);
+    });
+  });
+  
+  describe('assign()', function() {
+    it("assigns all enumerable (incl. inherited) props", function(){
+      expect(_.assign({a: 1, b: 2}, {b: 3, c: 4, d: 5}, {d: 6}))
+        .to.eql({a: 1, b: 3, c: 4, d: 6})
+    });
+  });
+  describe('merge()', function() {
+    it("recursively assign()", function(){
+      expect(_.merge({a: 1, b: 2}, {b: 3, nested: {c: 4, d: 5 }}, {d: 6}))
+        .to.eql({a: 1, b: 3, c: 4, d: 6})
+    });
+  });
+  
+  describe('flattenDeep()', function() {
+    it("recursively flattens", function(){
+      expect(_.flattenDeep([1,[2],[3,[4]]])).to.eql([1,2,3,4])
+    });
+  });
+  
+  describe("range()", function() {
+    it("creates array", function(){
+      expect(_.range(1, 11, 2)).to.eql([1, 3, 5, 7, 9])
+    });
+  });
+  describe("isFunction()", function(){
+    it("true for function", function() {
+      expect(_.isFunction(function(){})).to.eql(true)  
+    });
+    it("false for all other types", function() {
+      expect(_.isFunction(1)).to.eql(false)  
+      expect(_.isFunction({})).to.eql(false)  
+      expect(_.isFunction([])).to.eql(false)  
+    });    
+  });
+
+  describe("isNil()", function(){
+    it("true for null or undefined", function() {
+      expect(_.isNil(null)).to.eql(true)  
+      expect(_.isNil(undefined)).to.eql(true)  
+    });
+    it("false for all other types", function() {
+      expect(_.isNil(1)).to.eql(false)  
+      expect(_.isNil({})).to.eql(false)  
+      expect(_.isNil([])).to.eql(false)  
+      expect(_.isNil(false)).to.eql(false)  
+    });    
+  });
 
 
 })
--- underdash.ts
+++ underdash.ts
@@ -2,23 +2,308 @@ interface Iteraee {
   (element: any, index: number, collection: any[] | any): any
 }
 
+interface Reducer {
+  (accumulator:any, element: any, index: number, collection: any[] | any): any
+}
+
 class Underdash {
   /******************************************
    *
    * CORE COLLECTION METHODS
    ******************************************/
+  
   /**
-   * _.each()
+   * _.each
    * iterate through a collection
    */
-  each(collection: any[] | any, fn: Iteraee) {
+  each(collection: any[] | Object, fn: Iteraee): any {
     if (Array.isArray(collection)) {
-      for (let i = 0; i < collection.length; i++) {
-        fn(collection[i], i, collection);
-      }
-      return collection;
+      return _.arrayForEach(collection, fn);
+    } else if (_.isObject(collection)) {
+      return _.objectForEach(collection, fn);
     }
   }
+  
+  private arrayForEach(array: any[], fn) {
+    for (let i = 0; i < array.length; i++) {
+      fn(array[i], i, array);
+    }
+    return array;
+  }
+  
+  private objectForEach(object: Object, fn) {
+    for (let key in object) {
+      fn(object[key], key, object);
+    }
+    return object
+  }
+  
+  map(collection: any[] | Object, fn: Iteraee): any {
+    const result = [];
+    _.each(collection, function(value, key, collection){
+      if (!_.isNil(fn(value, key, collection))) {
+        result.push(fn(value, key, collection));  
+      }
+    })
+    return result;
+  }
+  
+  reduce(collection: any[], fn: Reducer, acc?: any): any {
+    if (!acc) {
+      acc = collection[0];
+      collection = collection.slice(1)
+    }
+    _.each(collection, function(value, key, collection){
+      acc = fn(acc, value, key, collection)
+    })
+    return acc;
+  }
+  
+  filter(collection: any[] | Object, fn: Iteraee): any {
+    return _.map(collection, function(value, key, collection){
+      if (fn(value, key, collection)) {
+        return value;
+      }
+    })
+  }
+  
+  reject(collection: any[] | Object, fn: Iteraee): any {
+    return _.filter(collection, function (value, key, collection) {
+      if (!fn(value, key, collection)) {
+        return value;
+      }
+    });
+  }
+  
+  every(collection: any[], fn: Iteraee): any {
+    return _.reduce(collection, function(acc, value, key, collection) {
+      return acc && Boolean(fn(value, key, collection));
+    }, true)
+  }
+  
+  some(collection: any[], fn: Iteraee): any {
+    return _.reduce(collection, function(acc, value, key, collection) {
+      return acc || Boolean(fn(value, key, collection));
+    }, true)
+  }
+  
+  // Clones one level
+  clone(input) {
+    if (_.isObject(input)) {
+      let clone = {};
+      _.each(input, function(value, key){
+        clone[key] = value;
+      });
+      return clone;
+    }
+    if (Array.isArray(input)) {
+      let clone = [];
+      _.each(input, function(value, key){
+        clone[key] = value;
+      });
+      return clone;
+    }
+    return input;
+  }
+  
+  // Recursively clones
+  cloneDeep(input) {
+    if (Array.isArray(input)) {
+      let clone = [];
+      _.each(input, (value, key) => {
+        clone[key] = this.cloneDeep(value);
+      });
+      return clone;
+    } 
+    if (_.isObject(input)) {
+      let clone = {};
+      _.each(input, (value, key) => {
+        clone[key] = this.cloneDeep(value);
+      });
+      return clone;
+    }
+    return input;
+  }
+  
+  isEqual(input1, input2) {
+    if (Array.isArray(input1) && Array.isArray(input2)) {
+      return _.isArrayEqual(input1, input2);
+    }
+    if (_.isObject(input1) && _.isObject(input2)) {
+      return _.isObjectEqual(input1, input2);
+    }
+    if (typeof input1 === typeof input2) {
+      return (input1 === input2);
+    }
+  }
+  
+  private isArrayEqual(array1, array2){
+    if (array1.length !== array2.length) { return false; }
+    return _.every(array1, (value, index) => {
+      return _.isEqual(value, array2[index]);
+    });
+  }
+  
+  private isObjectEqual(obj1, obj2){
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+      return false;
+    }
+    let equalFlag = true;
+    _.each(obj1, (value, key) => {
+      if (!_.isEqual(value, obj2[key])){
+        equalFlag = false
+      };
+    });
+    return equalFlag;
+  }
+  
+  /******************************************
+   *
+   * OBJECT METHODS
+   ******************************************/
+  
+  // Only enumerable properties
+  assign(destination, ...sources: Object[]) {
+    _.each(sources, function(source: Object){
+      _.each(source, function(value, key) {
+        destination[key] = value;
+      })
+    })
+    return destination;
+  }
+  
+  // Recursively does assign
+  merge(destination, ...sources: Object[]) {
+    let flattenObject = (object) => {
+      let result = {};
+      _.each(object, (unflattenedValue, key)=>{
+        if (!_.isObject(unflattenedValue)) {
+          return result[key] = unflattenedValue;
+        } else {
+          _.each(flattenObject(unflattenedValue), (value, key) => {
+            return result[key] = value;
+          })
+        }
+      })
+      return result;
+    }
+    
+    let flattenedSources = _.map(sources, (source) => {
+      return flattenObject(source);
+    });
+    
+    _.assign(destination, ...flattenedSources);
+    return destination;
+  }
+  
+  /******************************************
+   *
+   * ARRAY METHODS
+   ******************************************/
+   
+  /**
+   * Flattens one level deep
+   */
+  flatten(array: any[]): any {
+    let result = [];
+    _.each(array, function(value){
+      if (Array.isArray(value)) {
+        result = result.concat(value);
+      } else {
+        return result.push(value);
+      }
+    });
+    return result;
+  }
+  
+  /**
+   * Recursively flattens until there's no more nested array
+   */
+  flattenDeep(array: any[]): any {
+    let result = [];
+    _.each(array, (value) => {
+      if (Array.isArray(value)) {
+        result = result.concat(_.flattenDeep(value));
+      } else {
+        result.push(value);
+      }
+    });
+    return result;
+  }
+  
+   
+  /******************************************
+   *
+   * OTHER CORE METHODS
+   ******************************************/
+
+    range(start: number, end: number, step: number = 1) {
+      let result = [];
+      while (start < end) {
+        result.push(start);
+        start += step;
+      }
+      return result;
+    } 
+    
+   
+   debounce(fn, duration) {
+     return function (...args) {
+       let context = this;
+       let timeout;
+       if (timeout) {
+         clearTimeout(timeout);
+       }
+       timeout = setTimeout(function(){
+         fn.apply(context, args)
+       }, duration);
+     }
+   }
+   
+   throttle(fn, duration) {
+     return function () {
+       let isWaiting = false;
+       return function (...args) {
+        let context = this;
+        if (isWaiting) {
+          setTimeout(function(){
+            isWaiting = false;
+          }, duration)
+        } else {
+          fn.apply(context, args); 
+        }
+      }
+     }
+   }
+   
+  /******************************************
+   *
+   * HELPER METHODS
+   ******************************************/
+   /**
+    * _.isFunction
+    * only returns true if type is function
+    * lodash checks the toString() to avoid cross-browser issues
+    */
+  isFunction(input: any) {
+    return (typeof input === "function")
+  }
+  
+  /**
+   * _.isNil
+   * returns true if null or undefined
+   */
+  isNil(input: any) {
+    /**
+     * Possible to redefine undefined in old browsers (pre-ES5)
+     */
+    return (input == null)
+  }
+  
+  isObject(input: any) {
+    return (input.toString() === "[object Object]")
+  }
+  
 }
 
 export const _ = new Underdash;
