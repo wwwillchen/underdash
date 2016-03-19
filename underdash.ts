6069f5109b9538d9ab508e64e826e763142e208a
willchen90 <willchen90@gmail.com>
2016-03-19 10:10:39 -0700

    initial commit

---
+++ underdash.ts
@@ -0,0 +1,25 @@
+interface Iteraee {
+  (element: any, index: number, collection: any[] | any): any
+}
+
+class Underdash {
+  
+  /******************************************
+   * 
+   * CORE COLLECTION METHODS
+   ******************************************/
+  /**
+   * _.each()
+   * iterate through a collection
+   */
+  each (collection: any[] | any, fn: Iteraee) {
+    if (Array.isArray(collection)) {
+      for (let i = 0; i < collection.length; i++) {
+        fn(collection[i], i, collection);
+      }
+      return collection;
+    }  
+  }
+}
+
+export const _ = new Underdash;
