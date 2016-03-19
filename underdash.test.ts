e30d48fd36307cb6924bf55a6c2a261221beab4a
willchen90 <willchen90@gmail.com>
2016-03-19 10:12:54 -0700

    fmt

--- underdash.test.ts
+++ underdash.test.ts
@@ -1,38 +1,36 @@
-import {_} from "./underdash";
+import {_} from './underdash';
 import chai = require('chai');
 var expect = chai.expect;
 
-console.log("_", _)
-describe("Underdash", function(){
-  
-  it("exists", function(){
-    expect(typeof _).to.equal("object");
+console.log('_', _)
+describe('Underdash', function() {
+
+  it('exists', function() { expect(typeof _).to.equal('object'); });
+
+  describe('each()', function() {
+    it('works for arrays', function() {
+      const array = [];
+      let double = (x) => array.push(x * 2);
+      _.each([1, 2, 3], double);
+      expect(array).to.eql([2, 4, 6]);
+    });
   });
-  
-  describe("each()", function(){
-    it("works for arrays", function(){
-      const array = []
-      let double = (x) => array.push(x * 2)
-      _.each([1,2,3], double)
-      expect(array).to.eql([2,4,6])    
-    })
+
+  describe('map()', function() {
+
   });
-  
-  describe("map()", function(){
-    
-  });
-  
-  describe("reduce()", function(){
-    
+
+  describe('reduce()', function() {
+
   })
 
-  describe("filter()", function(){
-    
+  describe('filter()', function() {
+
   })
 
-  describe("reject()", function(){
-    
+  describe('reject()', function() {
+
   })
-  
-  
+
+
 })
--- underdash.ts
+++ underdash.ts
@@ -3,22 +3,21 @@ interface Iteraee {
 }
 
 class Underdash {
-  
   /******************************************
-   * 
+   *
    * CORE COLLECTION METHODS
    ******************************************/
   /**
    * _.each()
    * iterate through a collection
    */
-  each (collection: any[] | any, fn: Iteraee) {
+  each(collection: any[] | any, fn: Iteraee) {
     if (Array.isArray(collection)) {
       for (let i = 0; i < collection.length; i++) {
         fn(collection[i], i, collection);
       }
       return collection;
-    }  
+    }
   }
 }
 
